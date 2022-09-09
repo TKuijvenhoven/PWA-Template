ec8c6d3b1b5c50346a9cb82089e4adc6debbb019
TKuijvenhoven <94169966+TKuijvenhoven@users.noreply.github.com>
2022-09-09 18:55:37 +0200

    version 1.5

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
 window.addEventListener('load', () => {
-    getNews();
+   
     registerSW();
 });
-
-async function getNews() {
-    const res = await fetch(topHeadlinesUrl);
-    const json = await res.json();
-
-    const main = document.querySelector('main');
-
-    json.articles.forEach(article => {
-        const el = document.createElement('news-article');
-        el.article = article;
-        main.appendChild(el);
-    });
-}
 
 async function registerSW() {
     if ('serviceWorker' in navigator) {
